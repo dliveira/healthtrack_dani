0d4128264bbeea671285de400c06a5ca4b29a8a8
Danielle Oliveira <danielledeoliveiraoficial@gmail.com>
2020-09-11 18:04:22 -0300

    Projeto Criado

---
+++ src/test/java/com/healthtrack/demo/HealthtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthtrack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
