905f27af4f8ca8d49b58158a27de55bd5b5895f5
Danielle Oliveira <danielledeoliveiraoficial@gmail.com>
2020-09-11 23:38:42 -0300

    Banco de Dados Teste e criação automatica da base de dados a partir dos objetos

--- src/main/java/com/healthtrack/demo/domain/Plano.java
+++ src/main/java/com/healthtrack/demo/domain/Plano.java
@@ -3,9 +3,17 @@ package com.healthtrack.demo.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Plano implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String tipo;
 	private Date dataInicio;
