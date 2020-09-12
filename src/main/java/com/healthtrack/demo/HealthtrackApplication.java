b821177068613dbb8cc83ff2c223fbb0b2e2dfe0
Danielle Oliveira <danielledeoliveiraoficial@gmail.com>
2020-09-12 02:18:58 -0300

    Criando operacao de instanciacao

--- src/main/java/com/healthtrack/demo/HealthtrackApplication.java
+++ src/main/java/com/healthtrack/demo/HealthtrackApplication.java
@@ -1,13 +1,34 @@
 package com.healthtrack.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.healthtrack.demo.domain.Plano;
+import com.healthtrack.demo.repositories.PlanoRepository;
+
 @SpringBootApplication
-public class HealthtrackApplication {
+public class HealthtrackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PlanoRepository planoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthtrackApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Plano p1 = new Plano(null, "Nutricao", null, null, null);
+		Plano p2 = new Plano(null, "Exercicio", null, null, null);
+		
+		planoRepository.saveAll(Arrays.asList(p1, p2));
+	}
+
+	
 }
