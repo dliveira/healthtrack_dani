21c2a1690dd3923b2e44f3b56c0e862be64f6965
Danielle Oliveira <danielledeoliveiraoficial@gmail.com>
2020-09-11 22:48:58 -0300

    Testando a rimeira classe de dominio - Plano

---
+++ src/main/java/com/healthtrack/demo/domain/Plano.java
@@ -0,0 +1,94 @@
+package com.healthtrack.demo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Plano implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String tipo;
+	private Date dataInicio;
+	private Date dataTermino;
+	private Double valor;
+	
+	public Plano() {
+	}
+
+	public Plano(Integer id, String tipo, Date dataInicio, Date dataTermino, Double valor) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.dataInicio = dataInicio;
+		this.dataTermino = dataTermino;
+		this.valor = valor;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataTermino() {
+		return dataTermino;
+	}
+
+	public void setDataTermino(Date dataTermino) {
+		this.dataTermino = dataTermino;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Plano other = (Plano) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+}
--- src/main/java/com/healthtrack/demo/resources/PlanoResource.java
+++ src/main/java/com/healthtrack/demo/resources/PlanoResource.java
@@ -1,16 +1,29 @@
 package com.healthtrack.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.healthtrack.demo.domain.Plano;
 
 @RestController
 @RequestMapping(value="/planos")
 public class PlanoResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Plano> listar() {
+		
+		Plano p1= new Plano(1, "nutricao", null, null, null);
+		Plano p2= new Plano(2,"exercicio", null, null, null);
+		
+		List<Plano> lista = new ArrayList<>();
+		lista.add(p1);
+		lista.add(p2);
+		
+		return lista;
 	}
 	
 }
